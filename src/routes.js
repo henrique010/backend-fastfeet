24b3407eb4be112e0d5156966ce4c13124218765
Alfredo Henrique <alfredohenrique@alu.ufc.br>
2020-02-27 11:19:17 -0300

    file upload, created

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
 
 import Middleware from './app/middleware/auth';
+import multerConfig from './config/multer';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
@@ -13,5 +17,7 @@ routes.use(Middleware);
 
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients', RecipientController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
